038f9a291ed2b2212a5929d5a6500acee49cd21f
cindywxw <cindywen@uchicago.edu>
2017-08-28 12:47:59 -0500

    added license text to jflip

--- modules/jflip.cc
+++ modules/jflip.cc
@@ -1,6 +1,12 @@
 /*
  * jflip - whiten the input data by randomly inverting it around
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Copyright (C) 2010-2013 C Daniel Meliza <dan || meliza.org>
  */
 #include <iostream>
 #include <signal.h>
